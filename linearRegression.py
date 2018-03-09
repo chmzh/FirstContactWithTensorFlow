b94f8b3e934b169cfeb43356bef7ec8471019900
chmzh <28458942@qq.com>
2018-03-09 10:27:10 +0800

    Add files via upload

--- linearRegression.py
+++ linearRegression.py
@@ -2,15 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 NUM_POINTS = 1000
 vectors = []
+b1 = 0.3
+w1 = 0.1
 for i in range(NUM_POINTS):
     x1 = np.random.normal(0.0,0.55)
-    y1 = x1 * 0.1 + 0.3 * np.random.normal(0.0,0.05)
+    y1 = x1 * w1 + b1 + np.random.normal(0.0,0.05)
     vectors.append([x1,y1])
 x_data = [v[0] for v in vectors]
 y_data = [v[1] for v in vectors]
 
-plt.plot(x_data,y_data,'ro',label='original data')
-plt.legend()
-plt.show()
+# plt.plot(x_data,y_data,'ro',label='original data')
+# plt.legend()
+# plt.show()
 
+
+
+import tensorflow as tf
+W = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+b = tf.Variable(tf.zeros(1))
+y = W * x_data + b
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(8):
+        sess.run(train)
+        #W,b 逼近于 w1,b1
+        print(step,sess.run(W),sess.run(b))
 
