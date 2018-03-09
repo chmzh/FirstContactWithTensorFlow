9c90742bc1f72c2a2f7d9133649b0a2bdbe7b71f
chmzh <28458942@qq.com>
2018-03-09 17:16:47 +0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -24,9 +24,14 @@ vectors = tf.constant(conjunto_puntos)
 k = 4
 centroides = tf.Variable(tf.slice(tf.random_shuffle(vectors),[0,0],[k,-1]))
 #扩展维度
+# expanded_vectors = tf.expand_dims(vectors,1)
+# expanded_centroides = tf.expand_dims(centroides,0)
 expanded_vectors = tf.expand_dims(vectors,0)
 expanded_centroides = tf.expand_dims(centroides,1)
-assignments = tf.argmin(tf.reduce_sum(tf.square(tf.subtract(expanded_vectors,expanded_centroides)),2),0)
+diff = tf.subtract(expanded_vectors,expanded_centroides)
+square = tf.square(diff)
+reduce_sum = tf.reduce_sum(square,2)
+assignments = tf.argmin(reduce_sum,0)
 means = tf.concat([tf.reduce_mean(tf.gather(vectors, tf.reshape(tf.where( tf.equal(assignments, c)),[1,-1])), reduction_indices=[1]) for c in range(k)],0)
 update_centroides = tf.assign(centroides,means)
 
@@ -34,9 +39,9 @@ update_centroides = tf.assign(centroides,means)
 init_op = tf.global_variables_initializer();
 with tf.Session() as sess:
     sess.run(init_op)
-    # print(sess.run(assignments))
+    print(sess.run(square))
     # print("-------------")
-    # print(sess.run(expanded_centroides))
+    # print(sess.run(reduce_sum))
     # print("-------------")
     # print(sess.run(update_centroides))
     for step in range(100):
@@ -50,4 +55,4 @@ for i in range(len(assignment_values)):
 
 df = pd.DataFrame(data)
 sns.lmplot("x", "y", data=df, fit_reg=False, size=6, hue="cluster", legend=False)
-plt.show()
+# plt.show()
