f1b1978e45a8a287743bc98c44129e0385148091
chmzh <28458942@qq.com>
2018-03-22 15:51:48 +0800

    Add files via upload

---
+++ house/house_predicate.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import itertools
+
+import pandas as pd
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+#数据内容
+COLUMNS = ["crim", "zn", "indus", "nox", "rm", "age",
+           "dis", "tax", "ptratio", "medv"]
+FEATURES = ["crim", "zn", "indus", "nox", "rm",
+            "age", "dis", "tax", "ptratio"]
+LABEL = "medv"
+
+#定义输入函数处理程序
+def input_fn(data_set):
+  feature_cols = {k: tf.constant(data_set[k].values) for k in FEATURES}
+  labels = tf.constant(data_set[LABEL].values)
+  return feature_cols, labels
+
+
+def main(unused_argv):
+  # Load datasets
+  # 用pandas读取输入数据
+  training_set = pd.read_csv("boston_train.csv", skipinitialspace=True,
+                             skiprows=1, names=COLUMNS)
+  test_set = pd.read_csv("boston_test.csv", skipinitialspace=True,
+                         skiprows=1, names=COLUMNS)
+
+  # Set of 6 examples for which to predict median house values
+  prediction_set = pd.read_csv("boston_predict.csv", skipinitialspace=True,
+                               skiprows=1, names=COLUMNS)
+
+  # Feature cols
+  # 产生特征列的形式
+  feature_cols = [tf.contrib.layers.real_valued_column(k)
+                  for k in FEATURES]
+
+  # Build 2 layer fully connected DNN with 10, 10 units respectively.
+  regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
+                                            hidden_units=[10, 10],
+                                            model_dir="/tmp/boston_model")
+
+  # Fit
+  # 和前面说的一样用lambda来fit参数化的函数
+  regressor.fit(input_fn=lambda: input_fn(training_set), steps=5000)
+
+  # Score accuracy
+  ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+  loss_score = ev["loss"]
+  print("Loss: {0:f}".format(loss_score))
+
+  # Print out predictions
+  y = regressor.predict(input_fn=lambda: input_fn(prediction_set))
+  # .predict() returns an iterator; convert to a list and print predictions
+  # islice(y,6)是只看前6个结果
+  predictions = list(itertools.islice(y, 6))
+  print("Predictions: {}".format(str(predictions)))
+
+if __name__ == "__main__":
+  tf.app.run()
